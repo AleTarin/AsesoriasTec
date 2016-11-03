3a1574f446317ad6ddda8fed72c84a00cdd336f2
Alejandro Tarin <ale.tarin10@gmail.com>
2016-11-02 18:30:03 -0600

    First commit

---
+++ .c9/metadata/preview-https_/asesoriasbases-aletarin10.c9users.io
@@ -0,0 +1,1 @@
+{"filter":false,"title":"[P] https://asesoriasbases-aletarin10.c9users.io","undoManager":{"mark":-1,"position":-1,"stack":[]},"preview":{"path":"https://asesoriasbases-aletarin10.c9users.io","previewer":"preview.browser"},"timestamp":1462314206254,"hash":"67452301efcdab8998badcfe10325476c3d2e1f0"}
--- app/models/student.rb
+++ app/models/student.rb
@@ -2,5 +2,5 @@ class Student < ActiveRecord::Base
   belongs_to :lecture
   
   validates :student, presence: true
-  validates :valoration, numericality: {only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 5}
+  validates :valoration, numericality: {only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 10}
 end
